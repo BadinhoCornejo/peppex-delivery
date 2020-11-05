ec1aaf2dbb2a8fac9fe6996f4063c4c340901e3c
Badinho <Cornejo>
2020-11-05 18:37:41 -0500

    Removing files

--- lib/ui/components/top_app_bar.dart
+++ lib/ui/components/top_app_bar.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:peppex_delivery/constants/peppex_icons.dart';
-import 'package:peppex_delivery/services/api.dart';
 
 class TopAppBar extends StatelessWidget with PreferredSizeWidget {
-
   final String appBarText;
 
   TopAppBar({Key key, this.appBarText}) : super(key: key);
@@ -13,23 +11,17 @@ class TopAppBar extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(appBarText),
-      bottomOpacity: 0.0,
-      actions: [
-        IconButton(
+    return AppBar(title: Text(appBarText), bottomOpacity: 0.0, actions: [
+      IconButton(
           icon: Icon(Peppex.eva_search_outline),
-          onPressed: () { print(preferredSize.height); }
-        ),
-        IconButton(
-          icon: Icon(Peppex.ant_design_shopping_cart_outlined),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: Icon(Peppex.bytesize_filter),
-          onPressed: () {}
-        )
-      ]
-    );
+          onPressed: () {
+            print(preferredSize.height);
+          }),
+      IconButton(
+        icon: Icon(Peppex.ant_design_shopping_cart_outlined),
+        onPressed: () {},
+      ),
+      IconButton(icon: Icon(Peppex.bytesize_filter), onPressed: () {})
+    ]);
   }
 }
